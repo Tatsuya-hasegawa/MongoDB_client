1f8773aca7a533a4652fbdb82d5555fa5bc0e5c2
hackeT <40039738+Tatsuya-hasegawa@users.noreply.github.com>
2023-03-27 18:34:14 +0900

    Update dump_records2csv.py

--- dump_records2csv.py
+++ dump_records2csv.py
@@ -1,7 +1,7 @@
 import sys
 from os import environ as os_environ
 import pprint,traceback
-from pymongo import MongoClient
+from pymongo import MongoClient,CursorType
 from datetime import datetime
 import pandas as pd
 
@@ -9,14 +9,18 @@ def dump2csv(filename,records_json,counts):
 	try:
 		df_master = pd.DataFrame()
 		c=0
+		total = len(records_json)
 		for row_json in records_json:
 			del row_json["_id"]
 			if c==0:
 				print("### record schema check")
 				pprint.pprint(row_json)
+			if c%10000==0:
+				print("# [%s] dataframe conversion in progress: %d/%d"%(datetime.now().strftime('%Y-%m-%dT%H-%M-%SZ'),c,total))
+				pprint.pprint(row_json)
 			c+=1
 			df_json = pd.io.json.json_normalize(row_json)
-			df_master = pd.concat([df_master, df_json])
+			df_master = pd.concat([df_master, df_json],sort=False)
 		df_master.to_csv(filename, index=False, encoding='utf-8')
 		print("# Saved to %s"%filename)
 	except Exception as e:
@@ -29,7 +33,7 @@ def dump2csv(filename,records_json,counts):
 	else:
 		print("INTEGRITEY CHECK: FAILURE  (%d - %d = %d)"%(counts,file_rows-1,counts-file_rows-1))
 
-def pull_mongo_records(mongodb_url,database_name,table_name):
+def fetch_mongo_records(mongodb_url,database_name,table_name):
 	with MongoClient(mongodb_url) as client:
 		assert client is not None
 		print("# %s\n"%client)
@@ -40,7 +44,18 @@ def pull_mongo_records(mongodb_url,database_name,table_name):
 			counts = collection.count_documents({})
 			print("[%s](%s) %drecords"%(database_name,table_name,counts))
 
-			[records_json.append(record) for record in collection.find({})]
+			# faster fetch way from belows
+			# https://stackoverflow.com/questions/38647962/how-to-quickly-fetch-all-documents-mongodb-pymongo
+			# https://stackoverflow.com/questions/52904815/using-natural-sort-in-pymongo-mongodb
+			# without limit [ records_json.append(record) for record in collection.find({},cursor_type=CursorType.EXHAUST).sort([( '$natural', 1 )]) ]
+			position = 0
+			WINDOW_SIZE = 10000
+			while position < counts:
+				# E| Can't use limit and exhaust together.
+				[ records_json.append(record) for record in collection.find({}).sort([( '$natural', 1 )]).skip(position).limit(WINDOW_SIZE) ]
+				print("# [%s] position in progress: %d, stored record counts = %d"%(datetime.now().strftime('%Y-%m-%dT%H-%M-%SZ'),position,len(records_json)))
+				position += WINDOW_SIZE
+
 
 			# dump to csv file
 			print("\n # CSV dump phaise")
@@ -56,5 +71,5 @@ if __name__ == '__main__':
 	print("# connecting to %s\n"%target_mongo)
 	database_name = sys.argv[1]
 	table_name = sys.argv[2]
-	pull_mongo_records(target_mongo,database_name,table_name)
+	fetch_mongo_records(target_mongo,database_name,table_name)
 	print("\n# mongodb session closed.")
