23fd935c64d4cd022fdaf0cb8fc4fc20b1e8fbd2
hackeT <40039738+Tatsuya-hasegawa@users.noreply.github.com>
2023-03-28 14:53:49 +0900

    compatibility for pymongo 3.x and 4.x

--- list_databases_tables_counts.py
+++ list_databases_tables_counts.py
@@ -1,14 +1,25 @@
 from os import environ as os_environ
-from pymongo import MongoClient
+from pymongo import MongoClient, version as pymongo_version 
 
 def list_dbs(mongodb_url):
 	with MongoClient(mongodb_url) as client:
 		assert client is not None
 		print("# %s\n"%client)
-		for database_name in client.database_names():
+		print("# using pymongo version: %s"%pymongo_version)
+		if float(pymongo_version.replace(".","")) >= 400:
+			databases = client.list_databases()
+		else: 
+			databases = client.database_names()
+		for database_name in databases:
 			print("\nD|\t %s"%database_name)
+			if isinstance(database_name,dict):
+				database_name = database_name.get("name")
 			try:
-				for table_name in client[database_name].collection_names(include_system_collections=False):
+				if float(pymongo_version.replace(".","")) >= 370:
+					tables = client[database_name].list_collection_names()
+				else:
+					tables = client[database_name].collection_names(include_system_collections=False)
+				for table_name in tables:
 					counts = client[database_name][table_name].count_documents({})
 					print("T|\t\t - %s (%d records)"%(table_name,counts))
 			except Exception as e:
